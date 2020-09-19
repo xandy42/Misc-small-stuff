65cbafe53af5675234f4c525eae4cc8d65b4f876
xandy42 <tayloa18@tcnj.edu>
2020-09-19 12:20:48 -0400

    Clash Tier Calculator: Killed Try Except
    Made it better

--- clashtierswithchecking.py
+++ clashtierswithchecking.py
@@ -1,38 +1,56 @@
 import array
 import math
 
+toons = 0
+boss = 0
+suit = 0
 print ("Welcome! Enter the number of Toons.")
-while True:
-    try:   
-        toons = int(input()) #number of toons entering
-        if not toons in range (1,9):
-            print("Invalid number of Toons detected! Setting to default value of 4.")
-            toons = 4
-        print("\nWhat boss are you facing? (1 for VP, 2 for CFO, 3 for CLO, 4 for CEO)")
-        boss = int(input()) #boss selection
-        if not boss in range (1,5):
-            print("Invalid boss entry detected! Setting to default value of 1 (VP).")#invalid integer for boss, defaults to vp
-            boss = 1
-            
-        print("\nSUIT VALUE GUIDE: Tier 1 = 1, Tier 2 = 2, ... Tier 8 = 8")
-        print ("Please enter suit values for each Toon.")
+toonstring = str(input()) #number of toons entering
+if not toonstring.isnumeric():
+    print("Invalid number of Toons detected! Setting to default value of 4.") #invalid input string for toons, defaults to 4
+    toons = 4
+else:
+    toons = int(toonstring)
 
-        suitarray=array.array('i') #used to store suit values for all toons
-        total = 0
-        for i in range (0, toons):
-           suit = int(input())
-           if suit in range (1, 9):
-               suitarray.append(suit) #entering suit values into array
-               print("Entered.")
-           else: #integer too high or low, sets to default value of 8 
-               print("Invalid suit value detected! Setting to default value of 8.")
-               suit = 8
-               suitarray.append(suit)
-        break
-    except ValueError:
-        print("Invalid Input detected! You will need to re-enter all information, starting with number of Toons.")
+if not toons in range (1, 9):
+    print("Invalid number of Toons detected! Setting to default value of 4.") #invalid integer for toons, defaults to 4
+    toons = 4
+    
+print("\nWhat boss are you facing? (1 for VP, 2 for CFO, 3 for CLO, 4 for CEO)")
+bossstring = str(input()) #boss selection
+if not bossstring.isnumeric():
+    print("Invalid boss entry detected! Setting to default value of 1 (VP).") #invalid input string for boss, defaults to vp
+    boss = 1
+else:
+    boss = int(bossstring)
+
+if not boss in range (1, 5):
+    print("Invalid boss entry detected! Setting to default value of 1 (VP).") #invalid integer for boss, defaults to vp
+    boss = 1
+    
+            
+print("\nSUIT VALUE GUIDE: Tier 1 = 1, Tier 2 = 2, ... Tier 8 = 8")
+print ("Please enter suit values for each Toon.")
+
+suitarray=array.array('i') #used to store suit values for all toons
+total = 0
+for i in range (0, toons):
+    suitstring = str(input())
+    if suitstring.isnumeric():
+        suit = int(suitstring)
+    else: #invalid string, sets to default value of 8 
+        print("Invalid suit value detected! Setting to default value of 8.")
+        suit = 8
+        suitarray.append(suit)
+    if suit in range (1, 9):
+        suitarray.append(suit) #entering suit values into array
+        print("Entered.")
+    else: #invalid value, sets to default value of 8 
+        print("Invalid suit value detected! Setting to default value of 8.")
+        suit = 8
+        suitarray.append(suit)
 for i in range (0,toons):
-  total = total + suitarray[i] #used for total combined suit values
+  total += suitarray[i] #used for total combined suit values
 average = total/toons
 rounded = math.ceil(total/toons)
 
@@ -44,8 +62,8 @@ if boss == 3: #CLO
     print ("\nCLO difficulty/reward: \nTier 1 (4 C&D, Cogs up to Level 15, 40 sound gags, Level 8 max in final round, 1000HP): 1-3 \nTier 2 (6 C&D, Cogs up to Level 16, 35 sound gags, Level 9 max in final round, 1250HP): 4-6 \nTier 3 (8 C&D, Cogs up to Level 17, 30 sound gags, Level 10 max in final round, 1500HP): 7-8")
 if boss == 4: #CEO
     print("\nCEO difficulty/reward: \nTier 1 (5 fires, Cogs up to Level 17 in first round, Level 10 v2.0 Tier 6 Cogs in feeding round, 600HP): 1-3 \nTier 2 (7 fires, Cogs up to Level 18 in first round, Level 12 v2.0 Tier 7 Cogs in feeding round, 800HP): 4-6 \nTier 3 (9 fires, Cogs up to Level 19 in first round, Level 14 v2.0 Tier 8 Cogs in feeding round, 1000HP): 7-8")
-print("\nSuit average:", average)
-print ("Rounded average:", rounded) #prints average
+print("\nSuit average:", average) #average before rounding
+print ("Rounded average:", rounded) #prints average rounded, this is used for boss tiers 
 if rounded < 4:
     print ("Tier: 1")
 elif rounded < 7:
